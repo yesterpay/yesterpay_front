1398a8820c2c8c8b040e5689edda39300b0bb397
minju <betaa0528@gmail.com>
2024-11-16 17:30:31 +0900

    #YP-17 - 팀원 모집 페이지 UI 수정

--- lib/puzzle/team_list.dart
+++ lib/puzzle/team_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:practice_first_flutter_project/widgets/app_above_bar.dart';
 import 'package:practice_first_flutter_project/widgets/bottom_navigation_bar.dart';
 
 class TeamRecruitmentPage extends StatefulWidget {
@@ -10,13 +11,14 @@ class TeamRecruitmentPage extends StatefulWidget {
 }
 
 class _TeamRecruitmentPageState extends State<TeamRecruitmentPage> {
-  final emissionBtnColor = Color(0xFFFAB809);
-  final cancelBtnColor = Color(0xFF6E6053);
+  int currentIndex = 1;
+  final Color emissionBtnColor = Color(0xFFFAB809);
+  final Color cancelBtnColor = Color(0xFF6E6053);
 
   final List<Map<String, dynamic>> teamList = [
     {
       'teamName': '박민주팀 모집',
-      'memberCount': 5, // 현재 팀원의 수
+      'memberCount': 5,
       'joined': false,
     },
     {
@@ -27,7 +29,7 @@ class _TeamRecruitmentPageState extends State<TeamRecruitmentPage> {
     {
       'teamName': '한가연팀 모집',
       'memberCount': 5,
-      'joined': true, // 이미 가입된 팀
+      'joined': true,
     },
     {
       'teamName': '김민지팀 모집',
@@ -41,8 +43,8 @@ class _TeamRecruitmentPageState extends State<TeamRecruitmentPage> {
     },
   ];
 
-  static const int maxMembers = 6; // 모든 팀의 최대 인원
-  String? selectedTeam; // 사용자가 가입한 팀
+  static const int maxMembers = 6;
+  String? selectedTeam;
 
   void _showJoinConfirmationDialog(BuildContext context, String teamName) {
     showDialog(
@@ -73,70 +75,13 @@ class _TeamRecruitmentPageState extends State<TeamRecruitmentPage> {
                   ),
                 ),
               ),
-              SizedBox(
-                width: 10,
-              ),
+              SizedBox(width: 10),
               SizedBox(
                 width: 100,
                 child: TextButton(
                   onPressed: () {
                     Navigator.of(context).pop();
-                    _joinTeam(teamName); // 팀 가입
-                  },
-                  style: TextButton.styleFrom(
-                    backgroundColor: emissionBtnColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                  child: Text("확인"),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _showCreateTeamConfirmationDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("팀 생성 확인"),
-        content: Text("새로운 팀을 생성하시겠습니까?"),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
-        actions: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 100,
-                child: TextButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  style: TextButton.styleFrom(
-                    backgroundColor: cancelBtnColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                  child: Text(
-                    "취소",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              SizedBox(
-                width: 100,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                    _createTeam(); // 팀 생성
+                    _joinTeam(teamName);
                   },
                   style: TextButton.styleFrom(
                     backgroundColor: emissionBtnColor,
@@ -160,168 +105,169 @@ class _TeamRecruitmentPageState extends State<TeamRecruitmentPage> {
     });
   }
 
-  void _createTeam() {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text("새로운 팀이 생성되었습니다.")),
-    );
-    // 팀 생성 로직 추가
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('십자말 풀이 팀 모집'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.notifications),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        children: [
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Transform.scale(
-                  scale: 1.4,
-                  child: Image.asset(
-                    'assets/images/puzzleTitle.jpg', // 십자말 풀이 이미지
-                    width: MediaQuery.of(context).size.width * 0.3,
+      appBar: CustomAppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.2,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  // 양옆 배치
+                  Positioned(
+                    left: 30,
+                    child: Transform.scale(
+                      scale: 1.7,
+                      child: Image.asset(
+                        'assets/images/puzzleTitle.png',
+                        width: MediaQuery.of(context).size.width * 0.4,
+                      ),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.05,
-                ),
-                Transform.scale(
-                  scale: 1.2,
-                  child: Image.asset(
-                    'assets/images/friends.png', // 친구들 이미지
-                    width: MediaQuery.of(context).size.width * 0.45,
+                  Positioned(
+                    bottom: 15,
+                    right: 40,
+                    child: Transform.scale(
+                      scale: 1.35,
+                      child: Image.asset(
+                        'assets/images/friends.png',
+                        width: MediaQuery.of(context).size.width * 0.45,
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  // 위치를 조정하려면 Align이나 Padding을 사용
+                ],
+              ),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  onPressed: () => _showCreateTeamConfirmationDialog(context),
+            // 팀 생성 버튼
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: () {},
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.brown,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8),
                     ),
-                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 8,
+                    ),
                   ),
                   child: Text(
                     '팀 생성',
                     style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-          SizedBox(height: 16),
-          ...teamList.map((team) {
-            final isJoined = selectedTeam == team['teamName'];
-            final isFull = team['memberCount'] == maxMembers;
+            // 팀 리스트
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: teamList.length,
+              itemBuilder: (context, index) {
+                final team = teamList[index];
+                final isJoined = selectedTeam == team['teamName'];
+                final isFull = team['memberCount'] == maxMembers;
 
-            return Center(
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: Card(
-                  margin: EdgeInsets.symmetric(vertical: 8.0),
-                  color: Colors.amber[100],
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 14.0, vertical: 8.0),
+                  child: Card(
+                    color: Colors.amber[100],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
                               team['teamName'],
                               style: TextStyle(
                                   fontSize: 18, fontWeight: FontWeight.bold),
                             ),
-                            SizedBox(height: 8),
-                            Row(
-                              children: List.generate(maxMembers, (index) {
-                                return Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 4.0),
-                                  child: CircleAvatar(
-                                    backgroundColor: Colors.white,
-                                    child: index < team['memberCount']
-                                        ? Icon(Icons.person_outline_sharp
-                                            // isJoined
-                                            //     ? Icons.check_circle
-                                            //     : Icons.person,
-                                            // color: isJoined
-                                            //     ? Colors.green
-                                            //     : Colors.black,
-                                            )
-                                        : null,
-                                  ),
-                                );
-                              }),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              '${team['memberCount']}/$maxMembers 명',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            SizedBox(height: 8),
-                            ElevatedButton(
-                              onPressed: isJoined || isFull
-                                  ? null
-                                  : () => _showJoinConfirmationDialog(
-                                        context,
-                                        team['teamName'],
-                                      ),
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    isJoined ? Colors.grey : Colors.orange,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8),
+                          ),
+                          SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: List.generate(
+                              maxMembers,
+                              (index) => Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 4.0),
+                                child: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: index < team['memberCount']
+                                      ? Icon(Icons.person_outline)
+                                      : null,
                                 ),
                               ),
-                              child: Text(
-                                isJoined
-                                    ? '신청완료'
-                                    : isFull
-                                        ? '팀 가득참'
-                                        : '가입신청',
-                                style: TextStyle(color: Colors.white),
-                              ),
                             ),
-                          ],
-                        ),
-                      ],
+                          ),
+                          SizedBox(height: 6),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                '${team['memberCount']}/$maxMembers 명',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              ElevatedButton(
+                                onPressed: isJoined || isFull
+                                    ? null
+                                    : () => _showJoinConfirmationDialog(
+                                          context,
+                                          team['teamName'],
+                                        ),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      isJoined ? Colors.grey : Colors.orange,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                                child: Text(
+                                  isJoined
+                                      ? '신청완료'
+                                      : isFull
+                                          ? '팀 가득참'
+                                          : '가입신청',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ),
-            );
-          }),
-        ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
-      bottomNavigationBar: CustomBottomNavigationBar(currentIndex: 1,),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: currentIndex,
+      ),
     );
   }
 }
